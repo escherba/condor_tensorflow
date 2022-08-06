1f7f123c14b84197f141237110076a1556de059d
Eugene Scherba <escherba@gmail.com>
2022-08-06 11:07:03 -0700

    more tests

--- src/condor_tensorflow/tests/test_loss.py
+++ src/condor_tensorflow/tests/test_loss.py
@@ -50,3 +50,45 @@ def test_SparseOrdinalEarthMoversDistance() -> None:
     val = loss(tf.constant([2]), tf.constant([[-1., 1.]]))
     expect = tf.constant(1.5344467)
     tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
+
+
+def test_CondorNegLogLikelihoodMatch() -> None:
+    loss = CondorNegLogLikelihood()
+    val = loss(tf.constant([[1., 1.]]), tf.constant([[1., 1.]]))
+    expect = tf.constant(0.6265235)
+    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
+
+
+def test_SparseCondorNegLogLikelihoodMatch() -> None:
+    loss = CondorNegLogLikelihood(sparse=True)
+    val = loss(tf.constant([2.]), tf.constant([[1., 1.]]))
+    expect = tf.constant(0.6265235)
+    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
+
+
+def test_CondorOrdinalCrossEntropyMatch() -> None:
+    loss = CondorOrdinalCrossEntropy()
+    val = loss(tf.constant([[1., 1.]]), tf.constant([[1., 1.]]))
+    expect = tf.constant(0.9397845)
+    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
+
+
+def test_SparseCondorOrdinalCrossEntropyMatch() -> None:
+    loss = CondorOrdinalCrossEntropy(sparse=True)
+    val = loss(tf.constant([2]), tf.constant([[1., 1.]]))
+    expect = tf.constant(0.9397845)
+    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
+
+
+def test_OrdinalEarthMoversDistanceMatch() -> None:
+    loss = OrdinalEarthMoversDistance()
+    val = loss(tf.constant([[1., 1.]]), tf.constant([[1., 1.]]))
+    expect = tf.constant(0.73449475)
+    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
+
+
+def test_SparseOrdinalEarthMoversDistanceMatch() -> None:
+    loss = OrdinalEarthMoversDistance(sparse=True)
+    val = loss(tf.constant([2]), tf.constant([[1., 1.]]))
+    expect = tf.constant(0.73449475)
+    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
